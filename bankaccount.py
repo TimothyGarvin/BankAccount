e092764603a256867269eb4a94bab935871f90ee
Timothy Garvin <130323620+TimothyGarvin@users.noreply.github.com>
2023-05-02 17:31:33 -0700

    Revert "Edited to fit Users With Bank Accounts assignment"

--- bankaccount.py
+++ bankaccount.py
@@ -30,36 +30,9 @@ class BankAccount:
         else:
             print("Error: Broke")
         return self
-    
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
-        self.account = BankAccount(int_rate=1.2, balance=500)
-    
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
-        print(f"${self.account.balance}")
-        return self
 
-    def make_withdraw(self, amount):
-        self.account.withdraw(amount)
-        print(f"${self.account.balance}")
-        return self
-    
-    def display_user_balance(self):
-        self.account.display_account_info()
-        return self
-    
-    def interest(self):
-        self.account.yield_interest()
-        return self
+adriensAccount = BankAccount(1.2,500)
+sadiesAccount = BankAccount(1.5,600)
 
-# adrien = BankAccount(1.2,500)
-# sadie = BankAccount(1.5,600)
-
-adrien= User("Adrien", "adrienf@gmail.com")
-sadie= User("Sadie", "sadiem@gmail.com")
-
-adrien.make_deposit(30).make_deposit(50).make_deposit(20).make_withdraw(15).interest().display_user_balance()
-sadie.make_deposit(400).make_deposit(50).make_withdraw(30).make_withdraw(30).make_withdraw(60).make_withdraw(25).interest().display_user_balance()
+adriensAccount.deposit(30).deposit(50).deposit(20).withdraw(15).yield_interest().display_account_info()
+sadiesAccount.deposit(400).deposit(50).withdraw(30).withdraw(30).withdraw(60).withdraw(25).yield_interest().display_account_info()
